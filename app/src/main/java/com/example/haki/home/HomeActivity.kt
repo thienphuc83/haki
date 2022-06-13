b4613ffd9616cc64d65923a9584cc8ed6d33a32b
ThienPhuc <applehi@MacBook-Pro-cua-AppleHi.local>
2022-06-13 13:20:44 +0700

    config gradle

--- app/src/main/java/com/example/haki/home/HomeActivity.kt
+++ app/src/main/java/com/example/haki/home/HomeActivity.kt
@@ -1,15 +1,17 @@
 package com.example.haki.home
 
 import android.os.Bundle
-import android.util.Log
 import com.example.feature_utils.base.BaseActivity
 import com.example.haki.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        Log.d("nai","hh")
-        Log.d("nai","hh")
+
+        // custom bottomNavView
+        bottomNav.background = null
+        bottomNav.menu.getItem(2).isEnabled = false
     }
 }
