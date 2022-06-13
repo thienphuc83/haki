988a0e205cf6c15aa7aa50ccc81c4db9010785b6
ThienPhuc <applehi@MacBook-Pro-cua-AppleHi.local>
2022-06-13 11:00:14 +0700

    nai

--- app/src/main/java/com/example/haki/home/HomeActivity.kt
+++ app/src/main/java/com/example/haki/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.haki.home
 
 import android.os.Bundle
+import android.util.Log
 import com.example.feature_utils.base.BaseActivity
 import com.example.haki.R
 
@@ -8,5 +9,6 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Log.d("nai","hh")
     }
 }
